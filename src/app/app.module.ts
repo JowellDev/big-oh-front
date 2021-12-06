2a29e31ba09a7720c15b6a3371e2341a33202fc9
JowellDev <ephraimdigbeu@gmail.com>
2021-12-06 14:13:53 +0000

    finish first refactoring step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { AdminModule } from './admin/admin.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { ShareModule } from './shared/share.module';
 import { ArticlesModule } from './articles/articles.module';
+import { AdminModule } from './admin/admin.module';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
