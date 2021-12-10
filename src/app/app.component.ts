e6708fff9caa9aadfaa9832e737b0e67a3f8eb5d
jowell <joeldigbeu@arolitec.com>
2021-12-10 14:30:55 +0000

    change application name. Set bigoh to The big oh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'bigoh';
+  title = 'The bigoh';
 
   constructor(private authService: AuthService) {}
 
